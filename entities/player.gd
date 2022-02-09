7a08a231928a36d2c0ce6772cb01896520a7e323
Thor40 <nicholas.kosik@gmail.com>
2022-02-09 15:29:10 -0600

    merged master

--- entities/player.gd
+++ entities/player.gd
@@ -175,10 +175,7 @@ func add_gold(extragold: int):
 #
 func _attack():	
 	var mouse = get_node("CollisionShape2D").get_angle_to(get_global_mouse_position())
-#	var _attack_angle = mouse)
 	var _attack_angle  = rad2deg(mouse)
-#	a = wrapi(int(a), 0, 8)
-#	var _attack_angle  = int(a)
 	var weapon: = SCENE_WEAPON.instance()
 	weapon.add_to_group("weapons")
 	
@@ -190,7 +187,7 @@ func _attack():
 	weapon.position.y = 16
 	weapon.z_index = 11
 	
-	# Just make sure we attach Down always. We arn't doing any uppercuts!
+	# Just make sure we attach Down always. We aren't doing any uppercuts!
 	if _attack_angle < -90 or _attack_angle >90:
 		weapon.START_ANGLE = _attack_angle +45
 		weapon.END_ANGLE = _attack_angle -45
--- entities/weapon.gd
+++ entities/weapon.gd
@@ -5,12 +5,8 @@ var END_ANGLE = 45
 
 func _ready():
 	$AnimationPlayer.play("attack", -1, 5.0)
-#	if START_ANGLE > 0:
 	$AnimationPlayer.get_animation("attack").track_set_key_value(0, 0, START_ANGLE+90)
 	$AnimationPlayer.get_animation("attack").track_set_key_value(0, 1, END_ANGLE+90)
-#	else:
-#		$AnimationPlayer.get_animation("attack").track_set_key_value(0, 1, START_ANGLE)
-#		$AnimationPlayer.get_animation("attack").track_set_key_value(0, 0, END_ANGLE)
 		
 	print("start", START_ANGLE)
 	print("end", END_ANGLE)
