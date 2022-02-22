5a436c151cf1225d6fe433931c6f5bdb59e75812
Thor40 <nicholas.kosik@gmail.com>
2022-02-22 08:59:14 -0600

    char selector feature, incomplete

--- entities/Wizard.gd
+++ entities/Wizard.gd
@@ -8,7 +8,7 @@ extends "res://entities/player.gd"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	SCENE_WEAPON = load("res://entities/Daggers.tscn")
+	SCENE_WEAPON = load("res://entities/Wand.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
